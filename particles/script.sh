f7f8f4108cdc057e40a768a8ef3a64cf98299c0b
Carl Regårdh <calleisking@gmail.com>
2012-03-07 15:15:26 +0100

    med script

--- particles/script.sh
+++ particles/script.sh
@@ -17,9 +17,13 @@ do
 			for j in 1 2 4 8
 			do
 				if [ $k = 3 ]; then
-					mpirun -np $j ./${args[k]}Standard - n $i
+					mpirun -np $j ./${args[k]}Standard - n $i | grep "n = "
 				else
 					./${args[k]}Standard -n $i -p $j | grep "n = "
+				fi
+				
+				if [$k = 0 ]; then
+					break
 				fi
 			done
 		echo
@@ -30,9 +34,13 @@ do
 			for j in 1 2 4 8
 			do
 				if [ $k = 3 ]; then
-					mpirun -np $j ./${args[k]} - n $i
+					mpirun -np $j ./${args[k]} - n $i | grep "n = "
 				else
 					./${args[k]} -n $i -p $j | grep "n = "
+				fi
+				
+				if [$k = 0 ]; then
+					break
 				fi
 			done
 		echo
